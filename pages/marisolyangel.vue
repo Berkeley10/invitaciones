2167ed8f41be0b0f6edea5ae553cb683dd11d1d4
Angel Hernandez <angel_00hernandez@hotmail.com>
2024-09-16 10:40:16 -0600

    updates

--- pages/marisolyangel.vue
+++ pages/marisolyangel.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="relative  h-screen bg-cover bg-center bg-[url('/assets/images/3.jpg')]">
+  <div class="relative  h-screen bg-cover bg-center bg-[url('/assets/images/7.jpg')]">
 
     <!-- Overlay -->
     <div class="flex flex-col justify-center items-center h-full text-white text-5xl overlay ">
-      <div class="h-1/2 flex flex-col  pt-12 animate__animated animate__fadeIn"
+      <div class="h-1/2 flex flex-col  pt-12 animate__animated animate__fadeIn text-amber-50	"
         style="animation-delay: 400ms; animation-duration:900ms;">
         <span class="font1 sh ">SAVE the DATE</span>
-        <span class="font2 sh text-3xl text-center">04.01.25</span>
+        <span class="tangerine-regular sh text-3xl text-center">04.01.25</span>
 
       </div>
       <div class="h-1/2 flex justify-center items-center">
 
-        <span class="font2 sh animate__animated animate__fadeIn text-3xl"
-          style="animation-delay: 600ms; animation-duration:900ms;">Marisol y Ángel</span>
+        <span class="tangerine-regular sh animate__animated animate__fadeIn text-6xl text-amber-50	"
+          style="animation-delay: 600ms; animation-duration:900ms;">Marisol & Ángel</span>
 
       </div>
       <div class="mb-24 animate-bounce " style="animation-duration: 2800ms;">
         <svg class="size-10 " width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-  <polygon points="50,10 90,50 80,50 50,20 20,50 10,50" fill="white"/>
-  <polygon points="50,40 90,80 80,80 50,50 20,80 10,80" fill="white"/>
-</svg>
+          <polygon points="50,10 90,50 80,50 50,20 20,50 10,50" fill="white" />
+          <polygon points="50,40 90,80 80,80 50,50 20,80 10,80" fill="white" />
+        </svg>
 
       </div>
     </div>
@@ -31,16 +31,19 @@
   <div class=" w-full">
     <img src="/assets/images/Separador-Premium.png" alt="">
   </div>
-  <div class=" cormorant-garamond-light  bg-cover h-screen bg-[url('/assets/images/bg.jpg')]">
+  <div class="h-96 cormorant-garamond-light  bg-cover h-screen bg-[url('/assets/images/bg.jpg')]">
 
-    <div class="px-12">
-      <p class="text-center text-3xl">
+    <div class="px-12  h-[450px]">
+      <p class="text-center text-3xl animate__animated  hidden duration-1000	" style="animation-duration: 1000ms;"
+        id="text1">
         Hay momentos en la vida que marcan un antes y un después
       </p>
-      <p class="text-center text-3xl">
+      <p class="text-center text-3xl hidden animate__animated"
+        style="animation-duration: 1200ms; animation-delay: 800ms;" id="text2">
         Nos encantaría que nos acompañaras a celebrar el comienzo de nuestra vida juntos.
       </p>
-      <p class="text-center mt-12">
+      <p class="text-center mt-12 hidden animate__animated" style="animation-duration: 1800ms; animation-delay: 1200ms;"
+        id="text3">
         <span class="cormorant-garamond-bold-italic text-5xl text-center">NUESTRA BODA</span>
 
       </p>
@@ -53,8 +56,10 @@
     <div class=" text-center">
       <img src="/assets/images/Separador-Premium.png" alt="">
 
-      <div class="  h-96 bg-cover bg-top bg-[url('/assets/images/4.jpg')]  flex items-center">
-        <div class="grid grid-cols-4 glass text-white text-center h-suto  w-full rounded-xl mx-4  mt-44 pt-2">
+      <div class="  h-96 bg-cover bg-top bg-[url('/assets/images/8.jpg')]  flex items-center">
+        <div
+          class="grid grid-cols-4 glass text-white text-center w-full rounded-xl mx-4  mt-44 pt-2 animate__animated  hidden"
+          id="div1">
           <div class="col-span-4 sh"> <span class="text-5xl">Faltan</span>
 
           </div>
@@ -82,65 +87,126 @@
         </div>
       </div>
     </div>
-    <img src="/assets/images/Separador-Premium.png" alt="">
+    <div class="h-16">
 
-    <div class="flex flex-col text-xl justify-top h-screen bg-cover bg-[url('/assets/images/bg.jpg')]">
-      <div class="grid text-right  w-full">
-        <div>
+      <img class="absolute " src="/assets/images/Separador-Premium.png" alt="">
+    </div>
 
-          <!-- <img src="/assets/images/cal.png" alt="" class="max-w-12"> -->
+    <div class="flex flex-col text-xl justify-top   bg-cover bg-[url('/assets/images/bg.jpg')]">
+      <div class="min-h-[580px]">
+
+      
+      <div class="grid text-right  w-full ">
+     
+        <div class="flex w-full  justify-end items-center   animate__animated hidden" id="div2"
+          style="animation-duration: 1200ms;">
+          <img class="size-8 mt-6 mr-3" src="/assets/images/calendar-svgrepo-com.svg" alt="">
+          <div class="felx items-center h-full">
+
+            <span class="text-8xl text-rigth mr-4">04.01.25</span>
+          </div>
         </div>
-        <!-- <span class="text-4xl cormorant-garamond-bold-italic text-center pt-12">
-          FECHA
-        </span> -->
-        <span class="text-8xl text-rigth mr-4">04.01.25</span>
-        <span class="mr-4 mt-4">Código de Vestimenta</span>
-        <span class="mr-4 ">Elegancia en <span class="font-bold">negro</span>. Atuendo formal.</span>
+        <div class="div3  animate__animated hidden" id="div3"
+          style="animation-duration: 1200ms; animation-delay: 400ms">
+
+          <span class="mr-4 mt-4">Código de Vestimenta</span> <br>
+          <span class="mr-4 ">Elegancia en <span class="font-bold">negro</span>. Atuendo formal.</span>
+        </div>
 
       </div>
 
       <div class="text-left ml-4 grid mt-12">
-        <span class="text-5xl">Ceremonia</span>
-        <div>
+        <span class="text-5xl hidden  animate__animated" id="div4" style="animation-duration: 800ms;">Ceremonia</span>
+        <div class=" animate__animated hidden" id="div5" style="animation-duration: 1000ms; animation-delay: 400ms;">
           <span class=" text-4xl">04:00</span>
           <span>
             pm
           </span>
         </div>
-        <span class="mr-4 ">DIRECCION // DIRECCION</span>
-        <a href=""><span class="underline mr-4">Ver Ubicacion</span></a>
+        <div class=" animate__animated hidden" id="div6" style="animation-duration: 900ms; animation-delay: 700ms;">
+          <span class="mr-4  " >DIRECCION // DIRECCION</span> <br>
+          <a href="https://maps.app.goo.gl/ijJcEcgQ6x8d9Gj38 " target="_blank"><span class="underline mr-4">Ver
+              Ubicacion</span></a>
 
+        </div>
       </div>
       <div class="text-right mr-4 grid mt-12">
-        <span class="text-5xl">Recepción</span>
-        <div>
+        <span class="text-5xl  animate__animated hidden" id="div7" style="animation-duration: 900ms;">Recepción</span>
+        <div class=" animate__animated hidden" id="div8" style="animation-duration: 1200; animation-delay: 300ms;">
           <span class=" text-4xl">08:00</span>
           <span class="mr-4">
             pm
           </span>
         </div>
-        <span class="mr-4 ">DIRECCION // DIRECCION</span>
-        <a href=""><span class="underline mr-4">Ver Ubicacion</span></a>
+        <div class=" animate__animated hidden" id="div9" style="animation-duration: 500ms; animation-delay: 680ms;">
+
+          <span class="mr-4 ">DIRECCION // DIRECCION</span> <br>
+          <a href="https://maps.app.goo.gl/ijJcEcgQ6x8d9Gj38 " target="_blank"><span class="underline mr-4">Ver
+              Ubicacion</span></a>
+        </div>
 
       </div>
+    </div>
 
+      <div
+        class=" min-h-[800px] flex items-end justify-center  bg-center bg-cover  bg-[url('/assets/images/9.jpg')] mt-20">
+        <div class="glass py-6 mx-4 my-6 px-4 h-auto w-full   animate__animated hidden" id="div10">
+          <h2 class="text-2xl font-bold text-black mb-6 text-center">Confirma tu Asistencia</h2>
+          <form @submit.prevent="submitForm">
+            <div class="mb-4">
+              <label for="name" class="block text-black font-semibold">Nombre</label>
+              <input type="text" id="name" v-model="name"
+                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+                required>
+            </div>
+            <div class="mb-4">
+              <label for="email" class="block text-black font-semibold">Número de celular</label>
+              <input type="text" id="email" v-model="number"
+                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+                required>
+            </div>
+            <div class="text-center">
 
-      <!-- <div class="mt-24">
-        <div>
-          <span>CEREMONIA</span>
+              <label class="block text-black font-semibold mb-2 p-8 text-3xl">¿Asistirás?</label>
+            </div>
+            <div class="flex mb-4">
+              <div class="w-full">
+                <label class="inline-flex items-center">
+                  <input type="checkbox" name="confirm" v-model="attendanceYes"
+                    class="form-checkbox h-5 w-5 text-indigo-600" @change="attendanceNo = false">
+                  <span class="ml-2">Sí, asistiré</span>
+                </label>
+              </div>
+              <div class=" w-full">
+                <label class="inline-flex items-center">
+                  <input type="checkbox" name="confirm" v-model="attendanceNo"
+                    class="form-checkbox h-5 w-5 text-indigo-600" @change="attendanceYes = false">
+                  <span class="ml-2">No, no podré asistir</span>
+                </label>
+              </div>
+            </div>
+            <button type="submit"
+              class="w-full bg-black text-white py-2 rounded-lg hover:bg-white transition duration-300">Confirmar</button>
+          </form>
         </div>
-        <svg class="size-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-          <path
-          d="M408 120c0 54.6-73.1 151.9-105.2 192c-7.7 9.6-22 9.6-29.6 0C241.1 271.9 168 174.6 168 120C168 53.7 221.7 0 288 0s120 53.7 120 120zm8 80.4c3.5-6.9 6.7-13.8 9.6-20.6c.5-1.2 1-2.5 1.5-3.7l116-46.4C558.9 123.4 576 135 576 152l0 270.8c0 9.8-6 18.6-15.1 22.3L416 503l0-302.6zM137.6 138.3c2.4 14.1 7.2 28.3 12.8 41.5c2.9 6.8 6.1 13.7 9.6 20.6l0 251.4L32.9 502.7C17.1 509 0 497.4 0 480.4L0 209.6c0-9.8 6-18.6 15.1-22.3l122.6-49zM327.8 332c13.9-17.4 35.7-45.7 56.2-77l0 249.3L192 449.4 192 255c20.5 31.3 42.3 59.6 56.2 77c20.5 25.6 59.1 25.6 79.6 0zM288 152a40 40 0 1 0 0-80 40 40 0 1 0 0 80z" />
-        </svg>
-        
-      </div> -->
+      </div>
+      <div class="py-12 mx-4 min-h-96 text-center grid">
+        <span>Su compañía es el mejor regalo que podemos recibir.</span>
+
+
+        <span>Con cariño</span>
+
+
+        <span>Marisol y Ángel</span>
+      </div>
+    
     </div>
   </div>
 
 </template>
 <script setup>
 import 'animate.css';
+import { useFetch } from '#app'
 
 import { ref, onMounted, onBeforeUnmount } from 'vue'
 
@@ -149,6 +215,53 @@ const days = ref(0)
 const hours = ref(0)
 const minutes = ref(0)
 const seconds = ref(0)
+
+
+const name = ref('')
+const number = ref('')
+const attendanceYes = ref(false)
+const attendanceNo = ref(false)
+const itsPlay = ref(false)
+const confirmed =ref('')
+
+
+const submitForm = async () => {
+  try {
+    // Construir el objeto de datos para enviar
+    confirmed.value = attendanceYes.value ? true : attendanceNo.value ? false : null
+    const formData = {
+      name: name.value + '%' +  confirmed.value,
+      phone: number.value,
+
+    }
+
+    // Validar que se ha marcado alguna opción
+    if (formData.confirmed === null) {
+      alert('Por favor selecciona si asistirás o no.')
+      return
+    }
+
+    // Enviar la petición POST
+    const response = await useFetch('teinvito-b3dkbfewgubvc3e6.southcentralus-01.azurewebsites.net/api/Person', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    // Comprobar la respuesta
+    if (response.error) {
+      console.error('Error al enviar el formulario', response.error)
+    } else {
+      alert('Formulario enviado correctamente.')
+    }
+  } catch (error) {
+    console.error('Error en la solicitud', error)
+  }
+}
+
+
 
 const calculateTimeLeft = () => {
   const now = new Date()
@@ -169,9 +282,93 @@ const calculateTimeLeft = () => {
 
 let interval = null
 
+
+
+
+function animationsScroll() {
+
+  return () => {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop
+    if (scrollTop > 10) {
+      if (!itsPlay.value) {
+        itsPlay.value = true
+        console.log('play' + itsPlay.value)
+        const audio = new Audio('/song.mp3') // Ruta del archivo de audio en la carpeta public
+        audio.volume = 0.8
+        audio.play().catch(error => {
+          console.error("Error al reproducir el audio", error)
+
+        })
+      }
+
+
+    }
+    console.log(scrollTop)
+
+
+
+
+    if (scrollTop > 450) {
+      document.getElementById('text1').classList.remove('hidden')
+      document.getElementById('text2').classList.remove('hidden')
+      document.getElementById('text3').classList.remove('hidden')
+      document.getElementById('text1').classList.add('animate__fadeIn')
+      document.getElementById('text2').classList.add('animate__fadeIn')
+      document.getElementById('text3').classList.add('animate__fadeIn')
+    }
+    if (scrollTop > 1100) {
+      document.getElementById('div1').classList.remove('hidden')
+      document.getElementById('div1').classList.add('animate__fadeIn')
+
+    }
+    if (scrollTop > 1500) {
+      document.getElementById('div2').classList.remove('hidden')
+      document.getElementById('div3').classList.remove('hidden')
+      document.getElementById('div2').classList.add('animate__fadeInRight')
+      document.getElementById('div3').classList.add('animate__fadeInRight')
+    }
+    if (scrollTop > 1700) {
+      document.getElementById('div4').classList.remove('hidden')
+      document.getElementById('div5').classList.remove('hidden')
+      document.getElementById('div6').classList.remove('hidden')
+      document.getElementById('div4').classList.add('animate__fadeInLeft')
+      document.getElementById('div5').classList.add('animate__fadeInLeft')
+      document.getElementById('div6').classList.add('animate__fadeInLeft')
+    }
+    if (scrollTop > 1900) {
+      document.getElementById('div7').classList.remove('hidden')
+      document.getElementById('div8').classList.remove('hidden')
+      document.getElementById('div9').classList.remove('hidden')
+      document.getElementById('div7').classList.add('animate__fadeInRight')
+      document.getElementById('div8').classList.add('animate__fadeInRight')
+      document.getElementById('div9').classList.add('animate__fadeInRight')
+    }
+    if (scrollTop >2200) {
+      document.getElementById('div10').classList.remove('hidden')
+      
+      document.getElementById('div10').classList.add('animate__fadeInDown')
+      
+    }
+
+    
+
+  }
+}
+
+
 onMounted(() => {
   calculateTimeLeft()
   interval = setInterval(calculateTimeLeft, 1000)
+
+
+
+
+  const onScroll = animationsScroll()
+  window.addEventListener('scroll', onScroll)
+  onUnmounted(() => {
+    window.removeEventListener('scroll', onScroll)
+  })
+
 })
 
 onBeforeUnmount(() => {
@@ -193,10 +390,10 @@ onBeforeUnmount(() => {
 }
 
 .glass {
-  background: rgba(51, 51, 51, 0.384);
+  background: rgba(214, 214, 214, 0.616);
   box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.911);
-  backdrop-filter: blur(5px);
-  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
   border-radius: 10px;
   border: 1px solid rgba(29, 29, 29, 0.178);
 }
