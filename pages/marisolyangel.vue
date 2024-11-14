7b1f8f33a0e6d00d9b8221e756cf83f2f9831bc0
Angel Hernandez <angel_00hernandez@hotmail.com>
2024-11-13 20:46:22 -0600

    Update marisolyangel.vue

--- pages/marisolyangel.vue
+++ pages/marisolyangel.vue
@@ -357,7 +357,7 @@ const submitForm = async () => {
     // Enviar la petición POST
     const response = await fetch('https://tzty38v9ki.execute-api.us-east-2.amazonaws.com/dev/confirmations', {
       method: 'PUT',
-      mode: "Cors",
+      mode: "cors",
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'         // Aceptar JSON
